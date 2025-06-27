9d99509241af035e0f9ec3c5c02104bca3abbd43
bx <1947967840@qq.com>
2025-06-27 10:05:09 +0800

    build: 添加 Electron 构建和更新配置
    - 新增 electron-builder 配置文件，用于设置应用程序的构建参数
    - 添加 updater 配置文件，用于配置应用程序的自动更新功能
    - 配置文件包括了 Windows、macOS 和 Linux 平台的构建设置
    - 设置了应用程序的图标、版本号、版权声明等信息
    - 配置了 notarize、entitlements 等 macOS 特定设置
    - 配置了 AppImage、DMG 等 Linux 特定设置
    - 设置了发布到通用服务器的配置
    - 配置了使用 npmmirror 的 Electron 下载镜像

---
+++ config/electron-builder.config.js
@@ -0,0 +1,91 @@
+const pkg = require('../package.json')
+// const { execSync } = require('child_process');
+
+const productName = 'myApp'
+// 计算哈希值
+// const hash = execSync('git log -1 --format=%h', { encoding: 'utf8' }).trim();
+
+const config = {
+  appId: 'com.myApp.com',
+  productName,
+  copyright: `© 2025 ${pkg.author}`,
+  asar: true,
+  directories: {
+    output: 'build',
+    buildResources: 'build'
+  },
+  files: [
+    '!src/*',
+    '!temp/*',
+    '!config/*',
+    '!scripts/*',
+    '!.build/*',
+    '!frontend/*',
+    '!electron/*',
+    '!electron.vite.config.{js,ts,mjs,cjs}',
+    '!{**/.vscode/**,**/.idea/**,**/.cursor/**}',
+    '!{.eslintignore,.eslintrc.cjs,eslint.config*,.prettierignore,.prettierrc.*}',
+    '!{jsconfig.json,postcss.config.js,tailwind.config.js}',
+    '!{dev-app-update.yml,CHANGELOG.md,README.md,stats.html}',
+    '!{.env,.env.*,.npmrc,pnpm-lock.yaml}'
+  ],
+  asarUnpack: ['resources/**'],
+  win: {
+    icon: 'resources/icon.ico',
+    executableName: 'myApp',
+    target: ['nsis']
+  },
+  nsis: {
+    // https://builder.electron.js.cn/app-builder-lib.interface.nsisoptions
+    artifactName: `${productName} Setup ${pkg.version}.${'exe'}`,
+    // artifactName: `${productName}-${pkg.version}-setup-${hash}.${'exe'}`,
+    oneClick: false,
+    differentialPackage: true,
+    allowToChangeInstallationDirectory: true,
+    perMachine: true, // true=默认「所有用户」安装；false=默认「仅当前用户」安装，显示安装模式安装页面
+    deleteAppDataOnUninstall: false,
+    createDesktopShortcut: true,
+    createStartMenuShortcut: true,
+    shortcutName: 'myApp',
+    uninstallDisplayName: 'myApp',
+    allowElevation: true,
+    installerIcon: 'resources/icon.ico',
+    uninstallerIcon: 'resources/icon.ico',
+    installerHeaderIcon: 'resources/icon.ico',
+    include: 'resources/build/installer.nsh'
+  },
+  mac: {
+    entitlementsInherit: 'build/entitlements.mac.plist',
+    extendInfo: {
+      NSCameraUsageDescription: "Application requests access to the device's camera.",
+      NSMicrophoneUsageDescription: "Application requests access to the device's microphone.",
+      NSDocumentsFolderUsageDescription:
+        "Application requests access to the user's Documents folder.",
+      NSDownloadsFolderUsageDescription:
+        "Application requests access to the user's Downloads folder."
+    },
+    notarize: false
+  },
+  dmg: {
+    artifactName: `${pkg.name}-${pkg.version}.dmg`
+  },
+  linux: {
+    target: ['AppImage', 'snap', 'deb'],
+    maintainer: 'electronjs.org',
+    category: 'Utility'
+  },
+  appImage: {
+    artifactName: `${pkg.name}-${pkg.version}.AppImage`
+  },
+  publish: [
+    {
+      provider: 'generic',
+      url: 'http://'
+    }
+  ],
+  electronDownload: {
+    mirror: 'https://npmmirror.com/mirrors/electron/'
+  }
+}
+
+module.exports = config
