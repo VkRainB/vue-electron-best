b101bda139be7755d878a62821a0f2c19abdf07f
bx <1947967840@qq.com>
2025-06-27 10:05:41 +0800

    build: 优化项目构建流程和依赖管理
    - 移除了 main.js 中的多余导入
    - 更新了 package.json 中的脚本命令，增加了版本管理和优化了构建流程
    - 更新了多个依赖项版本，包括：
      - @vue/devtools -> @vitejs/plugin-vue-jsx
      - unocss -> 移除
      - rollup-plugin-visualizer -> 5.12.0
      - vite -> npm:rolldown-vite@^6.3.21
      - 新增 unplugin-element-plus 和 unplugin-vue-components

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,11 +3,6 @@ import App from './App.vue'
 // import { setupStore } from './store'
 import { setupRouter, router } from './router'
 import './assets/main.css'
-import '@unocss/reset/tailwind.css'
-import 'uno.css'
-// import { devtools } from '@vue/devtools'
-
-// process.env.NODE_ENV === 'development' && devtools.connect()
 
 const setup = async () => {
   const app = createApp(App)
